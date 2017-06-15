badb82f619805435be6789da7129397de501dbeb
Trevor Harwell <trevor.harwell@gmail.com>
2017-06-14 22:53:33 -0700

    Fixed issue with double quoted strings

--- index.js
+++ index.js
@@ -11,9 +11,16 @@ function injectEnvConfiguration(providedString, configuration) {
   if (jsonConfiguration == null) {
     jsonConfiguration = '{}';
   }
+  var inputString = String(providedString == null ? '' : providedString);
   var newPadding = runtimeEnvTemplate.length - jsonConfiguration.length;
   var finalString = jsonConfiguration + Array(Math.max(newPadding, 0)).join(' ');
-  return String(providedString == null ? '' : providedString).replace(runtimeEnvTemplate, finalString);
+  var templateStartIndex = inputString.indexOf(runtimeEnvTemplate);
+
+  if (templateStartIndex >= 0 && inputString[templateStartIndex - 1] === '"') {
+    finalString = finalString.replace(/"/g, '\\"');
+  }
+
+  return inputString.replace(runtimeEnvTemplate, finalString);
 };
 
 module.exports = injectEnvConfiguration;
--- test/index-test.js
+++ test/index-test.js
@@ -14,8 +14,19 @@ describe('index', function() {
     var expected = 'var test = \'{"hello":"world"}' + Array(runtimeEnvTemplate.length-17).join(' ') + '\';';
     injectEnvConfiguration(testString, { hello: 'world' }).should.equal(expected);
   });
+  it('replaces runtime template with values in double quote string', function() {
+    var testString = 'var test = "' + runtimeEnvTemplate + '";'
+    var expected = 'var test = "{\\"hello\\":\\"world\\"}' + Array(runtimeEnvTemplate.length-17).join(' ') + '";';
+    injectEnvConfiguration(testString, { hello: 'world' }).should.equal(expected);
+  });
   it('should be able to be evaluated', function() {
     var testString = 'JSON.parse(\'' + runtimeEnvTemplate + '\')';
+    var resolvedString = injectEnvConfiguration(testString, { hello: 'world' });
+    var result = eval(resolvedString);
+    result.should.have.property('hello', 'world');
+  });
+  it('should be able to be evaluated as a double quote', function() {
+    var testString = 'JSON.parse("' + runtimeEnvTemplate + '")';
     var resolvedString = injectEnvConfiguration(testString, { hello: 'world' });
     var result = eval(resolvedString);
     result.should.have.property('hello', 'world');
